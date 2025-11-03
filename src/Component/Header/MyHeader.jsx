968ab9e3be2abc1922e8ae2febebd5f062af1b13
yekta_akhavan <yekta.akhavan.dev@gmail.com>
2025-11-03 13:12:25 +0330

    update header

--- src/Component/Header/MyHeader.jsx
+++ src/Component/Header/MyHeader.jsx
@@ -1,11 +1,39 @@
-import "../../Component/Header/MyHeader.css"
+// import "../../Component/Header/MyHeader.css";
+// import Container from "react-bootstrap/esm/Container";
+// import LogoFull from "../../assets/images/logo/logo-full2.webp";
+
+// function MyHeader() {
+//   return (
+//     <>
+//       <header className=" text-light">
+//         <Container>
+//           <img src={LogoFull} alt="Yekta Akhavan Logo" className="logo" />
+//           <h1>Yekta Akhavan</h1>
+//           <p>Frontend Developer | Creative Technologist</p>
+//         </Container>
+//       </header>
+//     </>
+//   );
+// }
+
+// export default MyHeader;
+
+import "../../Component/Header/MyHeader.css";
 import Container from "react-bootstrap/esm/Container";
 import LogoFull from "../../assets/images/logo/logo-full2.webp";
 
 function MyHeader() {
   return (
     <>
-      <header className="bg-dark text-light">
+      <header className="galaxy-header">
+        {/* لایه Aurora */}
+        <div className="aurora"></div>
+
+        {/* پارتیکل‌ها */}
+        <div className="particles"></div>
+        <div className="stars"></div>
+
+
         <Container>
           <img src={LogoFull} alt="Yekta Akhavan Logo" className="logo" />
           <h1>Yekta Akhavan</h1>
