b5a98b9e2a6cb29e10cef4917084c8ce222acc4f
yekta_akhavan <yekta.akhavan.dev@gmail.com>
2026-06-27 17:20:38 +0330

    Add Vercel SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 
